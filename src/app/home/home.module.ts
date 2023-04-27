5a1ca74d9c4c4948f35602a6a3333aa21190691a
linlifxee <88711112+zhamalieva@users.noreply.github.com>
2023-04-27 13:38:13 +0600

    Add files via upload

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CorzinaComponent } from './corzina/corzina.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    CategoryComponent,
+    ProductComponent,
+    AboutComponent,
+    ContactComponent,
+    CorzinaComponent,
+
+  ],
+  exports: [
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
